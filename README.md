3d5dffb39d997768c55179adbb68f902d3c33800
Ax-47 <armingLwnza@gmail.com>
2025-12-30 19:49:17 +0700

    dfakldadsdadfsaf

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # nn
+
+https://youtube.com/playlist?list=PLZHQObOWTQDNU6R1_67000Dx_ZCJB-3pi&si=sQ4EVPFWXG_f9hWV
--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,22 @@ mod read_byte;
 fn main() -> PolarsResult<()> {
     let images = load_images("train-images-idx3-ubyte")?;
     let labels = load_labels("train-labels-idx1-ubyte")?;
-    let n_per_layers: Vec<usize> = vec![784, 16, 16, 10];
+    let n_per_layers: Vec<usize> = vec![784, 128, 16, 16, 16, 10];
     // let n_per_layers: Vec<usize> = vec![784, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 10];
-    // let n_per_layers: Vec<usize> = vec![784, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 10];
-    let mut nn = neural::NeuralNetwork::new(n_per_layers, 0.5);
+    let df = mnist_to_df(images.clone(), labels.clone(), 60)?;
+    let mut nn = neural::NeuralNetwork::new(n_per_layers, 0.05);
     for epoch in 0..10 {
         for batch in mnist_batch_iter(&images, &labels, 64, 60_000) {
             let df = batch?;
             nn.train(&df)?;
         }
+
+        nn.test(df.clone())?;
+
         if epoch == 3 {
             nn.set_learning_rate(0.005);
+            break;
         }
     }
-    let df = mnist_to_df(images, labels, 60)?;
-    nn.test(df.clone())?;
     Ok(())
 }
